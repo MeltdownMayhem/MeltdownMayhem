f8700c1054251c77aadf8d293f838ee8b7f457fc
Lorionn14 <129492475+Lorionn14@users.noreply.github.com>
2023-04-27 20:02:53 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package MeltdownMayhem;
+
+import javax.swing.JFrame;
+/**
+ * De Main class zorgt voor de Frame creation.
+ */
+public class Main {
+	
+	public static void main(String args[]) {
+		new Window();
+	}
+}
